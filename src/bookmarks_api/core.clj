bf52933066c5660e0eb84b4e63ccba92acb65ca0
Karel Lédl <karel-ledl@seznam.cz>
2026-08-15 12:51:34 +0200

    basic functions

--- src/bookmarks_api/core.clj
+++ src/bookmarks_api/core.clj
@@ -1,7 +1,29 @@
 (ns bookmarks-api.core
   (:gen-class))
 
+(def bookmarks (atom {}))
+(def bookmark-id (atom 0))
+
+(defn create-bookmark
+  [bookmark]
+  (swap! bookmarks assoc (inc @bookmark-id) bookmark))
+
+(defn read-bookmark
+  []
+  (println @bookmarks))
+
+(defn read-bookmark-by-id
+  [id]
+  (println (get @bookmarks id)))
+
+(defn delete-bookmark
+  [id]
+  (swap! bookmarks dissoc @bookmarks id))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (create-bookmark  {:id 1
+                     :title "Clojure Docs"
+                     :url "https://clojure.org"
+                     :tags ["clojure"]})
+  (read-bookmark))
