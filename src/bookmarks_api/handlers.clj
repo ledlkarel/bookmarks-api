549a149511b411c25393b8f36e0abc5ffec107a7
Karel Lédl <karel-ledl@seznam.cz>
2026-08-18 10:08:32 +0200

    bug fix

--- src/bookmarks_api/handlers.clj
+++ src/bookmarks_api/handlers.clj
@@ -1,10 +1,6 @@
 (ns bookmarks-api.handlers
   (:require [bookmarks-api.core :as bookmarks]))
 
-(defn list-bookmarks [_request]
-  {:status 200
-   :body (bookmarks/get-bookmarks)})
-
 (defn get-bookmark [request]
   (let [id (parse-long (get-in request [:path-params :id]))
         bookmark (bookmarks/get-bookmark id)]
